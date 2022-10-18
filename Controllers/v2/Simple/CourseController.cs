2c5638c8a7e8e2103370d313f597c6f747aabd1e
mebenal <59780051+mebenal@users.noreply.github.com>
2022-10-18 10:03:08 -0600

    Filter course by template

--- Controllers/v2/Simple/CourseController.cs
+++ Controllers/v2/Simple/CourseController.cs
@@ -33,7 +33,8 @@ namespace VmProjectBE.Controllers.v2
             [FromQuery] int? courseId,
             [FromQuery] string courseCode,
             [FromQuery] string courseName,
-            [FromQuery] int? resourceGroupTemplateId)
+            [FromQuery] int? resourceGroupTemplateId,
+            [FromQuery] int? vmTemplateId)
         {
             string bffPassword = _configuration.GetConnectionString("BFF_PASSWORD");
             bool isSystem = bffPassword == _vimaCookie;
@@ -46,7 +47,8 @@ namespace VmProjectBE.Controllers.v2
                     ("courseId", courseId),
                     ("courseCode", courseCode),
                     ("courseName", courseName),
-                    ("resourceGroupTemplateId", resourceGroupTemplateId));
+                    ("resourceGroupTemplateId", resourceGroupTemplateId),
+                    ("vmTemplateId", vmTemplateId));
                 switch (validParameters.Count)
                 {
                     case 0:
@@ -66,7 +68,20 @@ namespace VmProjectBE.Controllers.v2
                                     (from c in _context.Courses
                                      where c.CourseName == courseName
                                      select c).FirstOrDefault());
-
+                            case "vmTemplateId":
+                                return Ok(
+                                    (from vt in _context.VmTemplates
+                                     join vtt in _context.VmTemplateTags
+                                     on vt.VmTemplateId equals vtt.VmTemplateId
+                                     join t in _context.Tags
+                                     on vtt.TagId equals t.TagId
+                                     join tc in _context.TagCategories
+                                     on t.TagCategoryId equals tc.TagCategoryId
+                                     join c in _context.Courses
+                                     on t.TagName equals c.CourseCode
+                                     where tc.TagCategoryName == "Course"
+                                     where vt.VmTemplateId == vmTemplateId
+                                     select c).FirstOrDefault());
                             default:
                                 return BadRequest("Incorrect parameters entered");
                         }
