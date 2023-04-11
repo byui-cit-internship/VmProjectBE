3c95b005224a82a343a53643d123a0ae7c7ca50e
ooioiooGT <gilber1002@byui.edu>
2023-04-10 18:51:34 -0600

    add the get

--- Controllers/v2/Simple/NoteController.cs
+++ Controllers/v2/Simple/NoteController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using VmProjectBE.DAL;
 using VmProjectBE.Models;
+using VmProjectBE.DTO;
 
 namespace VmProjectBE.Controllers.v2
 {
@@ -26,8 +27,60 @@ namespace VmProjectBE.Controllers.v2
         {
 
         }
+        [HttpGet("")]
+        public async Task<ActionResult> GetNote(
+            [FromQuery] int notesId,
+            [FromQuery] string noteDetail,
+            [FromQuery] int  section)
+        {
+            string bffPassword = _configuration.GetConnectionString("BFF_PASSWORD");
+            bool isSystem = bffPassword == _vimaCookie;
+
+            User user = _auth.GetUser();
+
+            if (isSystem || user != null)
+            {
+                List<string> validParameters = QueryParamHelper.ValidateParameters(
+                    ("notesId", notesId),
+                    ("noteDetail", noteDetail),
+                    ("setion", section));
+                switch (validParameters.Count)
+                {
+                    case 0 : 
+                        return Ok(
+                            (from n in _context.Notes
+                            select n).ToList());
+                    case 1 : 
+                        switch (validParameters[0])
+                        {
+                            case "noteId":
+                                return Ok(
+                                    (from n in _context.Notes
+                                     where n.NotesId == notesId
+                                     select n).FirstOrDefault());
+                            case "noteDetail":
+                                return Ok(
+                                    (from n in _context.Notes
+                                     where n.NoteDetail == noteDetail
+                                     select n).FirstOrDefault());
+                                
+
+                                
+                        }
+                    default:
+                        return BadRequest("Incorrect parameters entered");
+
+                }
+
+            
+            }
+            else
+            {
+                return NotFound("Only the BFF application has access to this resource.");
+            }
+        }
         [HttpPost("")]
-        public async Task<ActionResult> PostNote([FromBody] Note note)
+        public async Task<ActionResult> PostNote([FromBody] NoteDTO noteDTO)
         {
             string bffPassword = _configuration.GetConnectionString("BFF_PASSWORD");
             bool isSystem = bffPassword == _vimaCookie;
@@ -38,9 +91,13 @@ namespace VmProjectBE.Controllers.v2
             {
                 try
                 {
-                    _context.Notes.Add(note);
+                    Note noteModel = new Note();
+                    noteModel.NoteDetail = noteDTO.NoteDetail;
+                    _context.Notes.Add(noteModel);
+                    var section = _context.Sections.FirstOrDefault(section=>section.SectionId==noteDTO.SectionId);
+                    noteModel.Section=section;
                     _context.SaveChanges();
-                    return Ok(note);
+                    return Ok(noteModel);
                 }
                 catch (Exception ex)
                 {
